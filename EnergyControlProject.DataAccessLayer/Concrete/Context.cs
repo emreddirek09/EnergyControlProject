a927578518517d7a5417ca9d559e0f82a97c1113
emredirek1905 <emredirek1905@gmail.com>
2024-01-26 01:03:50 +0300

    First Mig

---
+++ EnergyControlProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EnergyControlProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyControlProject.DataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.\\SQLEXPRESS; initial catalog=EnergyControlDb; integrated Security=true;  TrustServerCertificate=True");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
